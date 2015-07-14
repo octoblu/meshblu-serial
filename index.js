4c89608672f1000097e997699c3feeb2d8a2b967
Sqrt of Saturn <sqrt@octoblu.com>
2015-07-13 17:15:34 -0700

    Add delimiter, v1.2.0

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 'use strict';
 var util = require('util');
+var _ = require('lodash');
+var debug = require('debug')('meshblu-serial');
 var EventEmitter = require('events').EventEmitter;
 var serialport = require("serialport");
 var SerialPort = serialport.SerialPort
 
 var port = "COM4";
 var baud = 9600;
-
 
 var MESSAGE_SCHEMA = {
   type: 'object',
@@ -28,88 +29,112 @@ var OPTIONS_SCHEMA = {
     baud: {
       type: 'integer',
       required: true,
-      enum : [115200, 57600, 38400, 19200, 9600, 4800, 2400, 1800, 1200, 600, 300, 200, 150, 134, 110, 75,50]
-    }, 
+      enum : [115200, 57600, 38400, 19200, 9600, 4800, 2400, 1800, 1200, 600, 300, 200, 150, 134, 110, 75,50],
+      default: 57600
+    },
     delimiter : {
-      type : 'string'
+      type : 'string',
+      default: "\\n"
     }
   }
 };
 
 function Plugin(){
+  _.bindAll(this);
+
   this.options = {};
   this.messageSchema = MESSAGE_SCHEMA;
   this.optionsSchema = OPTIONS_SCHEMA;
+
   return this;
 }
 util.inherits(Plugin, EventEmitter);
 
-
-
 Plugin.prototype.onMessage = function(message){
   var self = this;
-  var payload = message.payload;
- // this.emit('message', {devices: ['*'], topic: 'echo', payload: payload});
 
-if(payload.readPorts == true){
+  if(!self.serialPort){
+    var error = new Error('no connected serialPort');
+    console.error(error);
+    self.emit('error', error);
+    return;
+  }
 
-self.serialPort.list(function (err, ports) {
-  ports.forEach(function(port) {
-    console.log(port.comName);
-    console.log(port.pnpId);
-    console.log(port.manufacturer);
+  var payload = message.payload || {};
 
-    self.emit("message", {devices: ['*'], "payload": ports
-             });
-  });
-});
+  if(payload.readPorts){
+    self.serialPort.list(function (err, ports) {
+      self.emit("message", {devices: ['*'], "payload": ports});
+    });
+  }
 
-  
-}
-
-  
-  self.serialPort.write(payload.serial_out, function(err, results) {
-    console.log('err ' + err);
-    console.log('results ' + results);
-  });
-
-
-
+  if(payload.serial_out){
+    self.serialPort.write(payload.serial_out);
+  }
 };
 
 Plugin.prototype.onConfig = function(device){
   var self = this;
-  self.setOptions(device.options||{});
+  self.cleanup(function(error){
+    if(error){
+      self.emit('error', error);
+    }
 
-  if(!this.options || !this.options.port){
-    var error = new Error('port field is required'); 
-    console.error(error); 
+    self.setOptions(device.options);
+    debug('options', self.options);
 
-  }
+    if(!self.options.port){
+      var error = new Error('port field is required');
+      console.error(error);
+      self.emit('error', error);
+      return;
+    }
 
-  var serialOptions = {
-    baudrate : self.options.baud || 57600
-  }; 
-
-  self.serialPort = new SerialPort(this.options.port,serialOptions);
-
-  self.serialPort.on("open", function () {
-    self.serialPort.on('data', function(data) {
-      console.log('data received: ' + data);
-      self.emit("message", {
-        devices: ['*'], 
-        "payload": {
-          "serial_in" : data.toString()
-        } 
-      });
-      
-      self.serialPort.flush(); 
+    self.serialPort = new SerialPort(self.options.port, {
+      baudrate : self.options.baud,
+      parser: serialport.parsers.readline(self.options.delimiter)
     });
-  });
+
+    self.serialPort.on("open", function(){
+      self.serialPort.on('data', self.onSerialData);
+    });
+  })
 };
 
 Plugin.prototype.setOptions = function(options){
-  this.options = options;
+  var self = this;
+  self.options = _.defaults(options, {
+    baud: 57600,
+    delimiter: "\\n"
+  });
+
+  self.options.delimiter = self.options.delimiter.replace(/\\n/g, "\n").replace(/\\r/g, "\r").replace(/\\d/g, "\d").replace(/\\t/g, "\t");
+};
+
+Plugin.prototype.onSerialData = function(data) {
+  var self = this;
+  debug('onSerialData', data.toString());
+
+  self.emit("message", {
+    devices: ['*'],
+    "payload": {
+      "serial_in" : data.toString()
+    }
+  });
+};
+
+Plugin.prototype.cleanup = function(callback){
+  var self = this;
+  callback = callback || _.noop;
+
+  if(!self.serialport){
+    return callback();
+  }
+
+  self.serialPort.close(function(error){
+    self.serialPort = null;
+    callback(error);
+  });
 };
 
 module.exports = {
